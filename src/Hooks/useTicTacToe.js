669454638f8798e6a67fbf85824ce8763e79b323
sukhendra523 <523sukh@gmail.com>
2024-04-28 14:01:38 +0530

    Fixed all issue and optimized useTicTacToe hook

--- src/Hooks/useTicTacToe.js
+++ src/Hooks/useTicTacToe.js
@@ -1,58 +1,76 @@
 import { useState } from "react";
 
-const initialBoard = new Array(9).fill(null);
-
+const initialBoard = () => Array(9).fill(null);
 
 const useTicTacToe = () => {
-    const [board, setBoard] = useState(initialBoard);
-    const [isXTurn, setIsXTurn] = useState(true);
+  const [board, setBoard] = useState(initialBoard());
+  const [isXTurn, setIsXTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
 
     const WINNING_PATTERNS = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ];
-    const currentPlayer = isXTurn ? 'X' : 'O';
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-    const calculateWinner =(currentBoard)=>{
-        WINNING_PATTERNS.forEach((winning_pattern)=>{
-            const [a,b,c]=winning_pattern;
-            if(currentBoard[a] && currentBoard[a]=== currentBoard[b] && currentBoard[a]===currentBoard[c])
-                return currentBoard[a]
-        })
+  const currentPlayer = isXTurn ? "X" : "O";
 
-        return null;
+  const calculateWinner = (currentBoard) => {
+    for (let i = 0; i < WINNING_PATTERNS.length; i++) {
+      const [a, b, c] = WINNING_PATTERNS[i];
+      if (
+        currentBoard[a] &&
+        currentBoard[a] === currentBoard[b] &&
+        currentBoard[a] === currentBoard[c]
+      ) {
+        return currentBoard[a];
+      }
     }
 
-    const handleClick = ( index )=>{
-        const updatedBoard = [...board]
-        updatedBoard[index] = currentPlayer;
+    return null;
+  };
 
-        const winner = calculateWinner(updatedBoard);
-        if(winner) return;
-        setIsXTurn(!isXTurn);
-        setBoard(updatedBoard)
+  const handleClick = (index) => {
+    // Retrun if winner already exists
+    if (winner) return;
+
+    const updatedBoard = [...board];
+    updatedBoard[index] = currentPlayer;
+    setBoard(updatedBoard);
+
+    const winnerName = calculateWinner(updatedBoard);
+
+    // If winner found set winner and return skip change the trun
+    if (winnerName) {
+      setWinner(winnerName);
+      return;
     }
+    // Change turn if no winner found but empty spaces are left
+    board.includes(null) && setIsXTurn(!isXTurn);
+  };
 
-    const getStatusMessage = ()=>{
-        const winner = calculateWinner(board);
-        if (winner) return`Player ${winner} wins!`;
-        if (!board.includes(null)) return `It's a draw`;
-        return `Player ${currentPlayer} Turn`
-    }
+  const getStatusMessage = () => {
+    if (winner) return `Player ${winner} wins!`;
 
-    const resetGame = ()=>{
-        setBoard(initialBoard);
-        setIsXTurn(true);
-    }
+    // if No more empty space are left then it's draw
+    if (!board.includes(null)) return `It's a draw`;
 
+    // If when nigther wins nore draw the tur changed
+    return `Player ${currentPlayer} Turn`;
+  };
 
-  return {board,handleClick,getStatusMessage,resetGame}
-}
+  const resetGame = () => {
+    setBoard(initialBoard);
+    setIsXTurn(true);
+    setWinner(null);
+  };
 
-export default useTicTacToe
+  return { board, handleClick, getStatusMessage, resetGame };
+};
+
+export default useTicTacToe;
