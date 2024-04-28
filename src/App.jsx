bc0499e9dd99ee755abdc022c472355616766390
sukhendra523 <523sukh@gmail.com>
2024-04-28 13:55:48 +0530

    Added TicTacToe Compnent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import TicTacToe from './components/TicTacToe'
 
 function App() {
 
   return (
     <div>
-     
+     <TicTacToe />
         
     </div>
   )
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,34 @@
+import useTicTacToe from "../Hooks/useTicTacToe";
+import './styles.scss';
+
+function TicTacToe() {
+  const {board, handleClick, resetGame, getStatusMessage} = useTicTacToe();
+
+  return (
+    <div className="game">
+      <div className="status">
+        {getStatusMessage()}
+        <button className="reset-button" onClick={resetGame}>
+          Reset Game
+        </button>
+      </div>
+
+      <div className="board">
+        {board.map((b, index) => {
+          return (
+            <button
+              className="cell"
+              key={index}
+              onClick={() => handleClick(index)}
+              disabled={b !== null}
+            >
+              {b}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TicTacToe;
